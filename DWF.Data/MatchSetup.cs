1daed9f3eb88e17a881b04f611b775cfdbcb8239
Josh Erkan <erkman.josh@gmail.com>
2021-04-13 18:39:51 -0400

    added wireframe and planning doc

---
+++ DWF.Data/MatchSetup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWF.Data
+{
+    public class MatchSetup
+    {
+        public int MatchSetupId { get; set; }
+
+        [ForeignKey (nameof(Player))]
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; }
+
+        public int NumberOfSets { get; set; }
+        public int NumberOfLegs { get; set; }
+    }
+}
---
+++ DWF.Data/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWF.Data
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       //public List<Match> Matches { get; set; }
+       //public List<Stats> Statistics { get; set; }
+        public int FiveOOne_AvgBest { get; set; }
+        public int FiveOOne_AvgWorst { get; set; }
+        public int CheckOutBest { get; set; }
+        public int CheckOutWorst { get; set; }
+
+    }
+}
