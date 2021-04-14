339692c7d4929309f5e8c66ebe61bfed2576766e
Josh Erkan <erkman.josh@gmail.com>
2021-04-13 20:41:26 -0400

    added Index view and added PlayerListItem Model

---
+++ DWF.Models/PlayerListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWF.Models
+{
+    public class PlayerListItem
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "501 Best Average")]
+        public int FiveOOne_AvgBest { get; set; }
+    }
+}
---
+++ DWF.WebMVC/Controllers/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using DWF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DWF.WebMVC.Controllers
+{
+    [Authorize]
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        public ActionResult Index()
+        {
+            var model = new PlayerListItem[0];
+            return View(model);
+        }
+    }
+}
